b7f399041ac0c154aba7b29176419d36ca4d738b
Robbie Clutton <robert.clutton@gmail.com>
2013-09-28 11:33:34 -0400

    Catchup on talks/posts

--- helpers/post_helpers.rb
+++ helpers/post_helpers.rb
@@ -32,6 +32,62 @@ module PostHelpers
         date: '28th June, 2013',
         uri: 'http://pivotallabs.com/the-four-goals-every-project-should-aspire-to-have',
         excerpt: 'I did a presentation recently at NYU for their Summer Launchpad programme where I talked about prototyping and agile development. There was one slide I touched upon in the talk that I wanted to expand upon here, and the topic was on the four goals every project should aspire to have.'
+      ),
+      Post.new(
+        title: 'To build a bookmarklet',
+        publication: 'pivotal',
+        date: '17th June, 2013',
+        uri: 'http://pivotallabs.com/to-build-a-bookmarklet/',
+        excerpt: 'Building a bookmarklet provides an interesting challenge. It involves interaction a website your application does not control where that site could be anything with any number of dependencies on CSS or Javascript libraries. The first choice to make is trying to work with that website and probably setting an !important on every CSS selector used and hope that there’s no namespace or versioning clashes with any Javascript included; or use an iframe.'
+      ),
+      Post.new(
+        title: 'API versioning',
+        publication: 'pivotal',
+        date: '31st May, 2013',
+        uri: 'http://pivotallabs.com/api-versioning/',
+        excerpt: 'How to version an API has been a thoroughly discussed topic in the last several years regardless of protocol or approach, be that SOAP, REST or Hypermedia. Why contribute another post to the topic? My last post on avoiding breaking changes through better design led to conversations in the office and online about versioning.'
+      ),
+      Post.new(
+        title: 'Stop leaky APIs',
+        publication: 'pivotal',
+        date: '15th May, 2013',
+        uri: 'http://pivotallabs.com/stop-leaky-apis',
+        excerpt: 'There are many blogs about how to expose an API for a Rails application and many times I look at this and am concerned about how these examples often leak the application design and the schema out through the API. When this leak occurs a change to the application internals can ripple out and break clients of an API, or force applications to namespace URI paths which I feel is unnecessary and ugly.'
+      ),
+      Post.new(
+        title: 'Stop leaking ActiveRecord throughout your application',
+        publication: 'pivotal',
+        date: '6th May, 2013',
+        uri: 'http://pivotallabs.com/stop-leaking-activerecord/',
+        excerpt: 'Extending ActiveRecord::Base leaks a powerful API throughout an application which can lead to tempting code which breaks good design. Take the classic blog example where you may want to retrieve the latest posts by a given author.'
+      ),
+      Post.new(
+        title: 'Single resource REST Rails routes',
+        publication: 'pivotal',
+        date: '16th April, 2013',
+        uri: 'http://pivotallabs.com/single-resource-rest-rails-routes/',
+        excerpt: 'REST principles by default is a fantastic convention within Rails applications. The documentation for how to route HTTP requests are comprehensive and give examples about photo resources within an application. If you’ve got photo and tag as first class resources of your application, Rails has you covered. But what if you are building an application with a focus on one type of resource, do you really want /resource_type as a prefix to all of your application paths? I certainly don’t and I’ll show you how to remove that without diverging from Rails core strenghts.'
+      ),
+      Post.new(
+        title: 'Guardian series reader',
+        publication: 'guardian',
+        date: '24th October, 2011',
+        uri: 'http://www.guardian.co.uk/info/2011/oct/24/guardian-beta-blogpost',
+        excerpt: 'Notes on a Guardian hack day project'
+      ),
+      Post.new(
+        title: 'Guardian big screen experience',
+        publication: 'guardian',
+        date: '21st October, 2011',
+        uri: 'http://www.guardian.co.uk/info/2011/oct/21/big-screen-experience',
+        excerpt: 'Notes on a Guardian hack day project'
+      ),
+      Post.new(
+        title: "Notes from 'Schema design at scale'",
+        publication: 'personal',
+        date: '19th September, 2011',
+        uri: 'http://blog.iclutton.com/2011/09/notes-from-schema-design-at-scale.html',
+        excerpt: 'Notes from the presentation'
       )
     ]
   end
--- helpers/talk_helpers.rb
+++ helpers/talk_helpers.rb
@@ -6,16 +6,45 @@ module TalkHelpers
   def talks
     [
       Talk.new(
-        title: 'Prototyping and agile development @ Pivotal Labs, Boulder',
+        title: 'Prototyping and agile development',
+        location: 'Denver Startup Week 2013',
         slug: 'prototyping-and-agile-development-pivotal-boulder',
         video_embed: '<iframe width="560" height="340" src="http://cdn.livestream.com/embed/pivotallabsboulder?layout=4&amp;clip=pla_7d118744-e994-4114-b917-07b3cd670cf6&amp;height=340&amp;width=560&amp;autoplay=false" style="border:0;outline:0" frameborder="0" scrolling="no"></iframe><div style="font-size: 11px;padding-top:10px;text-align:center;width:560px"><a href="http://www.livestream.com/pivotallabsboulder?utm_source=lsplayer&amp;utm_medium=embed&amp;utm_campaign=footerlinks" title="Watch pivotallabsboulder">pivotallabsboulder</a> on livestream.com. <a href="http://www.livestream.com/?utm_source=lsplayer&amp;utm_medium=embed&amp;utm_campaign=footerlinks" title="Broadcast Live Free">Broadcast Live Free</a></div>',
         deck_embed: '<script async class="speakerdeck-embed" data-id="f44063d00394013137722281cf926402" data-ratio="1.33333333333333" src="//speakerdeck.com/assets/embed.js"></script>'
       ),
       Talk.new(
-        title: 'Prototyping and agile development @ NYU Summer Launchpad Speaker Series 2013',
+        title: 'Prototyping and agile development',
+        location: 'NYU Summer Launchpad Speaker Series 2013',
         slug: 'prototyping-and-agile-development',
         video_embed: '<iframe width="640" height="360" src="http://www.youtube.com/embed/m8sGczJGsbA?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>',
-        deck_embed: '<script async class="speakerdeck-embed" data-id="1cf31790c1a2013094e842d55316ac74" data-ratio="1.2994923857868" src="//speakerdeck.com/assets/embed.js"></script>')
+        deck_embed: '<script async class="speakerdeck-embed" data-id="1cf31790c1a2013094e842d55316ac74" data-ratio="1.2994923857868" src="//speakerdeck.com/assets/embed.js"></script>'
+      ),
+      Talk.new(
+        title: 'Sustainable small architecture',
+        location: 'QCon, London 2013',
+        slug: 'sustainable-small-architecture',
+        video_embed: '<object type="application/x-shockwave-flash" data="http://d1snlc0orfrhj.cloudfront.net/static/flash/svmplayer.swf" width="320" height="265" id="player" style="visibility: visible; display: none !important;"><param name="allowscriptaccess" value="always"><param name="allowfullscreen" value="true"><param name="wmode" value="opaque"><param name="flashvars" value="server=rtmpe://video.infoq.com/cfx/st/&amp;streams=mp4:presentations/13-mar-startuparchitecture.mp4,0"></object>',
+        deck_embed: '<iframe class="speakerdeck-iframe" frameborder="0" src="//speakerdeck.com/player/245d61c06a150130302422000a9d039d?" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" style="border: 0px; background-color: transparent; margin: 0px; padding: 0px; border-top-left-radius: 5px; border-top-right-radius: 5px; border-bottom-right-radius: 5px; border-bottom-left-radius: 5px; width: 920px; height: 752.0000000000017px; background-position: initial initial; background-repeat: initial initial;"></iframe>'
+      ),
+      Talk.new(
+        title: 'Getting your confidence back',
+        location: 'London Ruby User Group (LRUG) 2013',
+        slug: 'getting-your-confidence-back',
+        deck_embed: '<iframe class="speakerdeck-iframe" frameborder="0" src="//speakerdeck.com/player/4c107240698e01308a0122000a1d8cd9?" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" style="border: 0px; background-color: transparent; margin: 0px; padding: 0px; border-top-left-radius: 5px; border-top-right-radius: 5px; border-bottom-right-radius: 5px; border-bottom-left-radius: 5px; width: 920px; height: 752.0000000000017px; background-position: initial initial; background-repeat: initial initial;"></iframe>'
+      ),
+      Talk.new(
+        title: 'Sustainable BDD',
+        location: 'LA Ruby Conf, 2013',
+        slug: 'sustainable-bdd',
+        video_embed: '<iframe width="640" height="360" src="http://www.youtube.com/embed/j7An19XQwBg?feature=player_detailpage" frameborder="0" allowfullscreen=""></iframe>',
+        deck_embed: '<iframe class="speakerdeck-iframe" frameborder="0" src="//speakerdeck.com/player/23e21da069580130145e123138156442?" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" style="border: 0px; background-color: transparent; margin: 0px; padding: 0px; border-top-left-radius: 5px; border-top-right-radius: 5px; border-bottom-right-radius: 5px; border-bottom-left-radius: 5px; width: 920px; height: 752.0000000000017px; background-position: initial initial; background-repeat: initial initial;"></iframe>'
+      ),
+      Talk.new(
+        title: 'API patterns and practices',
+        location: 'Hacks and Hackers meetup',
+        slug: 'api-patterns-and-practices',
+        deck_embed: '<iframe src="http://www.slideshare.net/slideshow/embed_code/13026910" width="427" height="356" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen="" webkitallowfullscreen="" mozallowfullscreen=""> </iframe>'
+      )
     ]
   end
 end
